aa06343bcdb791757fcaa614da9c178bb45a3e1a
ginogipsy <francesco.grossi92@gmail.com>
2022-01-10 12:06:12 +0100

    fixato Es59

--- Il Linguaggio C - Libro/Capitolo5/Esercizio59/Es59.c
+++ Il Linguaggio C - Libro/Capitolo5/Esercizio59/Es59.c
@@ -13,9 +13,9 @@ int main(void)
     double costoParcheggio3 = 0.0;
     double costoTotale = 0.0;
 
-    
+
         printf("%s", "Inserisci le ore dei tre clienti: ");
-        scanf("%f%f%f", &oreParcheggio1, &oreParcheggio2, &oreParcheggio3);
+        scanf("%lf%lf%lf", &oreParcheggio1, &oreParcheggio2, &oreParcheggio3);
 
         oreTotali = oreParcheggio1+oreParcheggio2+oreParcheggio3;
 
@@ -24,11 +24,11 @@ int main(void)
         costoParcheggio3 = calcolaCostoParcheggio(oreParcheggio3);
         costoTotale = costoParcheggio1 + costoParcheggio2 + costoParcheggio3;
 
-        printf("%10s%10s%10s:\n", "Car, Hours, Total");
-        printf("%10.2f%10.2f%10.2f", 1, oreParcheggio1, costoParcheggio1);
-        printf("%10.2f%10.2f%10.2f", 2, oreParcheggio2, costoParcheggio2);
-        printf("%10.2f%10.2f%10.2f", 3, oreParcheggio3, costoParcheggio3);
-        printf("%10s%10.2f%10.2f","TOTAL", oreTotali, costoTotale);
+        printf("%10s%10s%10s:\n", "Car", "Hours", "Charges");
+        printf("%10d%10.2f%10.2f\n", 1, oreParcheggio1, costoParcheggio1);
+        printf("%10d%10.2f%10.2f\n", 2, oreParcheggio2, costoParcheggio2);
+        printf("%10d%10.2f%10.2f\n", 3, oreParcheggio3, costoParcheggio3);
+        printf("%10s%10.2f%10.2f\n","TOTAL", oreTotali, costoTotale);
 }
 
 double calcolaCostoParcheggio(double oreParcheggio)
@@ -38,7 +38,7 @@ double calcolaCostoParcheggio(double oreParcheggio)
     {
         return 2.0;
     }
-    
+
     costoTotale += 2.0;
     oreParcheggio -= 3.0;
 
@@ -47,7 +47,7 @@ double calcolaCostoParcheggio(double oreParcheggio)
        oreParcheggio -= 1.0;
        costoTotale += 0.5;
     }*/
-    costoTotale = costoTotale +((int) oreParcheggio % 1 * 0.5);
+    costoTotale = costoTotale +((int) oreParcheggio / 1 * 0.5);
 
     if(oreParcheggio - (int) oreParcheggio != 0)
     {
