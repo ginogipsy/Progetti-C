21b8bddf189579ee611be017193b14bb57f092b3
francesco grossi <francesco.grossi92@gmail.com>
2022-02-03 00:11:05 +0100

    Ese89 avviato

--- Il Linguaggio C - Libro/Capitolo8/Esercizio87/Es87.c
+++ Il Linguaggio C - Libro/Capitolo8/Esercizio87/Es87.c
@@ -1,8 +1,21 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#include <stdlib.h>
+#define SIZE 90
 
 int main(void)
 {
+    printf_s("%s\n","Inserisci 4 numeri in virgola mobile e li sommo per te");
+    double somma = 0.0;
+    for(size_t i=0;i<4;i++)
+    {
+        printf_s("\n%s%d%s","Inserisci il ",i+1," in virgola mobile: ");
+        char inserimento[SIZE];
+        char *endStr;
+        fgets(inserimento, SIZE, stdin);
+        somma = somma + strtod(inserimento, &endStr);
+    }
 
+    printf_s("\n%s%.2f","Somma dei valori inseriti: ", somma);
 }
---
+++ Il Linguaggio C - Libro/Capitolo8/Esercizio89/Es89.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+#include <string.h>
+
+#define SIZE 90
+
+int main(void)
+{
+    char sentences[2][SIZE];
+    printf_s("%s\n","Insert 2 sentences and i say if 1st is bigger, ");
+}
